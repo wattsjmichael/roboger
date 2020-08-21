0a26dd0ff5e1a5fa99aef887a1c1d55d16312625
Michael Watts <wattsjmichael@gmail.com>
2020-08-21 09:33:25 -0700

    replace all 1's with Beep and show to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
+function robogerTranslate(number){
+    let output =[];
+    for (i =0; i<=number; i++){
+        if (i.toString().includes(1)){
+            output.push("beep!");
+            } else {
+                output.push(i);
+                }
+ 
+        }
+        console.log(output);
+        return output;
+    };
+        
+
+
 $(document).ready(function(){
     $("#formOne").submit(function(event){
         event.preventDefault();
-        const userInput =$("input#number").val();
-        let newArray = [userInput]     
-        console.log(newArray);
+        
+        let userInput =$("#numberVal").val();
+        let newArray = robogerTranslate(userInput);
+        
+        $("#result").text(newArray);
+        $("#result").show();
     });
 });
