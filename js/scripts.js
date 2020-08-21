b8c3b98ab6b1af5fa374f4f189a1d9ca0e8917b7
Michael Watts <wattsjmichael@gmail.com>
2020-08-21 11:20:15 -0700

    switched logic of robogerTranslate. (13) now translates to wont you be my neightbor

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function robogerTranslate(number){
     let output =[];
     for (i =0; i<=number; i++){
-        if (i.toString().includes(1)){
-                output.push("beep!");
+        if (i.toString().includes(3)){
+                output.push("Wont you be my neighbor?");
             } else if (i.toString().includes(2)) {
                 output.push("boop!")
-            } else if (i.toString().includes(3)) {
-                output.push("Wont you be my neighbor?")
+            } else if (i.toString().includes(1)) {
+                output.push("Beep!")
             } else {
                 output.push(i);
                 }
